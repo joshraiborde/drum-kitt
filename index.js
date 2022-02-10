e5238d024b96b99606c8d0d634aa30a7286b2e9c
joshraiborde <joshraiborde@gmail.com>
2022-02-10 17:59:00 -0500

    added for loop to be able to click on all drum buttons

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
- alert("this works");
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//     alert("i got clicked!")
+// }
+
+// document.querySelector("button").addEventListener("click", function(){
+//     alert("w was clicked!")
+// })
+
+// document.querySelectorAll("button").addEventListener("click", function(){
+//     alert("i was clicked!")
+// })
+
+// CHALLENGE: add a click event to all the buttons.
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    alert("i was clicked!")
+})
+
+}
